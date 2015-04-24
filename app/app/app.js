590bbb5cae75c66306a43d302b317a06edf043cd
Mike Klemarewski <mikek@mobify.com>
2015-04-24 11:15:19 -0700

    Reset app.js

--- app/app/app.js
+++ app/app/app.js
@@ -3,23 +3,31 @@ require([
     'bluebird',
     'plugins/applicationPlugin',
     'plugins/webViewPlugin',
-    'plugins/anchoredLayoutPlugin'
+    'plugins/anchoredLayoutPlugin',
+    'plugins/headerBarPlugin',
+    'plugins/drawerPlugin'
 ],
 function(
-    Astro,
-    Promise,
-    ApplicationPlugin,
-    WebViewPlugin,
-    AnchoredLayoutPlugin
+     Astro,
+     Promise,
+     ApplicationPlugin,
+     WebViewPlugin,
+     AnchoredLayoutPlugin,
+     HeaderBarPlugin,
+     DrawerPlugin
 ) {
 
     // Enter your site url here
-    var baseUrl = 'http://www.google.com/';
+    var baseUrl = "http://<your local IP address>:5000/";
 
     // Initialize plugins
     var applicationPromise = ApplicationPlugin.init();
     var mainWebViewPromise = WebViewPlugin.init();
     var layoutPromise = AnchoredLayoutPlugin.init();
+    var headerPromise = HeaderBarPlugin.init();
+    var drawerPromise = DrawerPlugin.init();
+    var cartWebViewPromise = WebViewPlugin.init();
+
 
     // Start the app at the base url
     mainWebViewPromise.then(function(mainWebView) {
@@ -36,8 +44,50 @@ function(
         application.setMainInputPlugin(mainWebView.address);
     });
 
-    Promise.join(applicationPromise, layoutPromise, function(application, layout) {
-        application.setMainViewPlugin(layout.address);
+    // Create a new promise for when icons have been loaded into the header bar
+    var loadIconPromise = headerPromise.then(function(headerBar){
+        return Promise.join(
+            headerBar.setRightIcon(baseUrl + "/images/bag.png"),
+            headerBar.setCenterIcon(baseUrl + "/images/logo.png"),
+            headerBar.setBackgroundColor("#FFFFFF")
+        );
+    });
+
+    // Add the header bar to the top of the layout
+    Promise.join(layoutPromise, headerPromise, loadIconPromise, function(layout, headerBar) {
+        layout.addTopView(headerBar.address);
+        headerBar.show();
+    });
+
+    // Set the layout plugin as the main content view of the drawer
+    Promise.join(drawerPromise, layoutPromise, function(drawer, layout) {
+        drawer.setContentView(layout.address);
+    });
+
+    Promise.join(drawerPromise, applicationPromise, function(drawer, application) {
+        application.setMainViewPlugin(drawer.address);
+    });
+
+    cartWebViewPromise.then(function(webView) {
+        webView.navigate(baseUrl + "cart/")
+    });
+
+    // Add the cart web view to the right drawer
+    var rightDrawerPromise = Promise.join(cartWebViewPromise, drawerPromise, function(cartWebView, drawer) {
+        var rightDrawer = drawer.initRightMenu(cartWebView.address);
+        return rightDrawer;
+    });
+
+    Promise.join(rightDrawerPromise, headerPromise, function(rightDrawer, header) {
+        header.on('rightIconClick', function() {
+            rightDrawer.toggle();
+        });
+    });
+
+    Promise.join(mainWebViewPromise, rightDrawerPromise, function(mainWebView, rightDrawer) {
+        mainWebView.on('addToCartClicked', function() {
+            rightDrawer.open();
+        });
     });
 
 }, undefined, true);
