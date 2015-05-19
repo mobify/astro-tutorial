f1876461b8bb562fdbd6a1fcaf863f3799577180
Mike Klemarewski <mikek@mobify.com>
2015-05-19 13:08:32 -0700

    Update app.js for end of tutorial

--- app/app/app.js
+++ app/app/app.js
@@ -18,13 +18,10 @@ function(
 ) {
 
     // Enter your site url here
-    var BASE_URL = 'http://10.10.1.63:5000/';
-    var CART_PATH = '/cart';
-    var ACCOUNT_PATH = '/account';
+    var BASE_URL = 'http://<Your IP Address>:5000/';
+    var CART_URL = BASE_URL + 'cart/';
     var LOGO_URL = BASE_URL + '/images/velo.png';
-    var ACCOUNT_ICON_URL = BASE_URL + '/images/account.png';
     var CART_ICON_URL = BASE_URL + '/images/cart.png';
-    var BACK_ICON_URL = BASE_URL + '/images/back.png';
 
     // Initialize plugins
     var applicationPromise = ApplicationPlugin.init();
@@ -33,18 +30,18 @@ function(
     var headerPromise = HeaderBarPlugin.init();
     var drawerPromise = DrawerPlugin.init();
     var cartWebViewPromise = WebViewPlugin.init();
-    var accountWebViewPromise = WebViewPlugin.init();
+
 
     // Start the app at the base url
     mainWebViewPromise.then(function(mainWebView) {
         mainWebView.navigate(BASE_URL);
     });
 
-    accountWebViewPromise.then(function(accountWebView) {
-        accountWebView.navigate(BASE_URL + 'account/');
+    cartWebViewPromise.then(function(webView) {
+        webView.navigate(CART_URL);
     });
 
-    // Use mainWebView as the main content view for our layout
+    // Use the mainWebView as the main content view for our layout
     Promise.join(layoutPromise, mainWebViewPromise, function(layout, mainWebView) {
         layout.setContentView(mainWebView.address);
     });
@@ -57,7 +54,6 @@ function(
     // Create a new promise for when icons have been loaded into the header bar
     var loadIconPromise = headerPromise.then(function(headerBar){
         return Promise.join(
-            headerBar.setLeftIcon(ACCOUNT_ICON_URL),
             headerBar.setRightIcon(CART_ICON_URL),
             headerBar.setCenterIcon(LOGO_URL),
             headerBar.setBackgroundColor('#FFFFFF')
@@ -79,101 +75,15 @@ function(
         application.setMainViewPlugin(drawer.address);
     });
 
-    cartWebViewPromise.then(function(webView) {
-        webView.navigate(BASE_URL + 'cart/');
-    });
-
-    var getPathFromUrl = function(url) {
-        var parsedUrl = document.createElement('a');
-        parsedUrl.href = url;
-        return parsedUrl.pathname;
-    };
-
-    Promise.join(mainWebViewPromise, headerPromise, drawerPromise,
-        function(mainWebView, header, drawer) {
-
-            var swapLeftMenuIcon = function(path) {
-                if (path !== '/') {
-                    header.setLeftIcon(BACK_ICON_URL);
-                    header.off('leftIconClick');
-                    header.on('leftIconClick', function() {
-                        mainWebView.back();
-                    });
-                } else {
-                    header.setLeftIcon(ACCOUNT_ICON_URL);
-                    header.off('leftIconClick');
-                    header.on('leftIconClick', function() {
-                        drawer.toggleLeftMenu();
-                    });
-                }
-            };
-
-            mainWebView.on('navigate' , function(params) {
-                var path = getPathFromUrl(params.url);
-                swapLeftMenuIcon(path);
-            });
-
-            mainWebView.on('back', function(params) {
-                var path = getPathFromUrl(params.url);
-                swapLeftMenuIcon(path);
-            });
-    });
-
-    // Make sure the cart web view doesn't navigate anywhere but the cart
-    Promise.join(cartWebViewPromise, mainWebViewPromise, drawerPromise,
-        function(cartWebView, mainWebView, drawer) {
-            cartWebView.disableDefaultNavigationHandler()
-            cartWebView.on('navigate', function(params) {
-                var url = params.url;
-                var path = getPathFromUrl(url);
-
-                if (path === CART_PATH) {
-                    cartWebView.navigate(url);
-                    return;
-                }
-                mainWebView.events.trigger('navigate', {url: url});
-                drawer.hideRightMenu();
-            });
-    });
-
-    // Make sure the account webview sends it's navigation to the main web view
-    Promise.join(accountWebViewPromise, mainWebViewPromise, drawerPromise,
-        function(accountWebView, mainWebView, drawer) {
-            accountWebView.disableDefaultNavigationHandler()
-            accountWebView.on('navigate', function(params) {
-                var url = params.url;
-                var path = getPathFromUrl(url);
-
-                if (path === ACCOUNT_PATH) {
-                    accountWebView.navigate(url);
-                    return;
-                }
-                mainWebView.events.trigger('navigate', {url: url});
-                drawer.hideLeftMenu();
-            });
-    });
-
     // Add the cart web view to the right drawer
     var rightDrawerPromise = Promise.join(cartWebViewPromise, drawerPromise, function(cartWebView, drawer) {
         var rightDrawer = drawer.initRightMenu(cartWebView.address);
         return rightDrawer;
     });
 
-    // Add the account web view to the left drawer
-    var leftDrawerPromise = Promise.join(accountWebViewPromise, drawerPromise, function(accountWebView, drawer) {
-        var leftDrawer = drawer.initLeftMenu(accountWebView.address);
-        return leftDrawer;
-    });
-
     Promise.join(rightDrawerPromise, headerPromise, function(rightDrawer, header) {
         header.on('rightIconClick', function() {
             rightDrawer.toggle();
-        });
-    });
-
-    Promise.join(leftDrawerPromise, headerPromise, function(leftDrawer, header) {
-        header.on('leftIconClick', function() {
-            leftDrawer.toggle();
         });
     });
 
