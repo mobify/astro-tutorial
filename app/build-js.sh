3ba88c040ce8b4b09b3d6bed8e081311c0de4fb0
Liz Cross <elizabeth@mobify.com>
2016-12-02 13:27:28 -0800

    Update android gradle with new astro config. Update build-js to newest.

--- app/build-js.sh
+++ app/build-js.sh
@@ -2,9 +2,9 @@
 
 set -e
 
+echo "Setting up path variables and finding node"
 MYPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-ROOT=$MYPATH/..
-EXTRA_NPM_ARGS="--no-progress --no-spin"
+EXTRA_NPM_ARGS=""
 EXTRA_GRUNT_ARGS=""
 
 function findNode() {
@@ -15,11 +15,6 @@ if [ "$1" == "--no-color" ]; then
     EXTRA_NPM_ARGS="--no-color $EXTRA_NPM_ARGS"
     EXTRA_GRUNT_ARGS="--no-color $EXTRA_GRUNT_ARGS"
 fi
-
-pushd "$MYPATH"
-
-# Force supporting Homebrew installations of npm.
-export PATH=$PATH:/usr/local/bin
 
 if ! findNode; then
     echo "Cannot find 'npm'. Trying via nvm..."
@@ -39,20 +34,17 @@ if ! findNode; then
     fi
 fi
 
-if ! findNode; then
-    # Force supporting Homebrew installations of npm.
-    export PATH=$PATH:/usr/local/bin
-fi
+# Force supporting Homebrew installations of npm.
+export PATH=$PATH:/usr/local/bin
 
 if ! findNode; then
     echo "Cannot find 'npm'. Aborting. Add your npm path to \`user-env.sh\` and retry."
     exit 1
 fi
 
-# Build app.js.
-pushd $ROOT
-npm install $EXTRA_NPM_ARGS
-
+echo "Building app.js"
 pushd $MYPATH
-$MYPATH/node_modules/grunt-cli/bin/grunt $EXTRA_GRUNT_ARGS build
+    $MYPATH/node_modules/.bin/grunt $EXTRA_GRUNT_ARGS build
 popd
+
+echo "SUCCESS: build app.js"
