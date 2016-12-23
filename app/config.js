379db6587633edf5b52dd550aed8686405e7b4cf
Liz Cross <elizabeth@mobify.com>
2016-12-23 15:39:53 -0800

    Rename astro-sdk to mobify-progressive-app-sdk

--- app/config.js
+++ app/config.js
@@ -1,13 +1,13 @@
 require.config({
     baseUrl: '.',
     paths: {
-        'astro-base': '../node_modules/astro-sdk/js/src/astro-base',
-        'astro-full': '../node_modules/astro-sdk/js/src/astro-full',
-        'application': '../node_modules/astro-sdk/js/src/application',
+        'astro-base': '../node_modules/mobify-progressive-app-sdk/js/src/astro-base',
+        'astro-full': '../node_modules/mobify-progressive-app-sdk/js/src/astro-full',
+        'application': '../node_modules/mobify-progressive-app-sdk/js/src/application',
         'bluebird': '../node_modules/bluebird/js/browser/bluebird',
-        'plugins': '../node_modules/astro-sdk/js/src/plugins',
-        'plugin-manager': '../node_modules/astro-sdk/js/src/plugin-manager',
-        'vendor/backbone-events': '../node_modules/astro-sdk/js/vendor/backbone-events',
-        'worker': '../node_modules/astro-sdk/js/src/worker'
+        'plugins': '../node_modules/mobify-progressive-app-sdk/js/src/plugins',
+        'plugin-manager': '../node_modules/mobify-progressive-app-sdk/js/src/plugin-manager',
+        'vendor/backbone-events': '../node_modules/mobify-progressive-app-sdk/js/vendor/backbone-events',
+        'worker': '../node_modules/mobify-progressive-app-sdk/js/src/worker'
     }
 });
